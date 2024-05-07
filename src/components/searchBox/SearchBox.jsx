591f4951c4cd35dd5e00194d846c064b0e8440ed
Solnichna <143127001+Solnichna@users.noreply.github.com>
2024-05-07 16:26:54 +0200

    Part3

--- src/components/searchBox/SearchBox.jsx
+++ src/components/searchBox/SearchBox.jsx
@@ -1,26 +1,39 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { setSearchTerm } from '../../redux/filtersSlice'; 
+import { useDispatch, useSelector } from "react-redux";
+import { changeFilterName } from "../../redux/filters/filtersSlice.js";
+import { changeFilterNumber } from "../../redux/filters/filtersSlice.js";
 
-const SearchBox = () => {
+export const SearchBox = () => {
   const dispatch = useDispatch();
-  const searchTerm = useSelector(state => state.filters.name);
+  const selectFilterName = useSelector((state) => state.filters.name);
+  const selectFilterNumber = useSelector((state) => state.filters.name);
 
-  const handleSearch = event => {
-    dispatch(setSearchTerm(event.target.value));
+  const handleInputChangeName = (event) => {
+    dispatch(changeFilterName(event.target.value));
+  };
+
+  const handleInputChangeNumber = (event) => {
+    dispatch(changeFilterNumber(event.target.value));
   };
 
   return (
-    <div className="search-box">
+    <div className="search-box-container">
       <p>Find contacts by name</p>
       <input
+        onChange={handleInputChangeName}
+        value={selectFilterName}
         type="text"
-        placeholder="Search"
-        value={searchTerm}
-        onChange={handleSearch}
+        placeholder="Search..."
+        className="search-box-input"
+      />
+
+      <p>Find contacts by number</p>
+      <input
+        onChange={handleInputChangeNumber}
+        value={selectFilterNumber}
+        type="text"
+        placeholder="Search..."
         className="search-box-input"
       />
     </div>
   );
 };
-
-export default SearchBox;
---
+++ src/components/userMenu/UserMenu.jsx
@@ -0,0 +1,22 @@
+import { useDispatch, useSelector } from "react-redux";
+
+import { LogOut } from "../../redux/auth/operations.js";
+import { selectUser } from "../../redux/auth/selectors.js";
+
+export const UserMenu = () => {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+
+  return (
+    <div className="container-user">
+      <p className="user-name">Name: {user.name}</p>
+      <button
+        type="button"
+        onClick={() => dispatch(LogOut())}
+        className="logout-button"
+      >
+        Logout
+      </button>
+    </div>
+  );
+};
